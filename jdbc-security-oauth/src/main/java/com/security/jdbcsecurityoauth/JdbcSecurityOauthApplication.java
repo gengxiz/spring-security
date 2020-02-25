91eb3ad4aeb120db16d55b651a5895b795d06e1b
liyhu <2539517371@qq.com>
2020-02-25 11:12:27 +0800

    添加jdbc模块

---
+++ jdbc-security-oauth/src/main/java/com/security/jdbcsecurityoauth/JdbcSecurityOauthApplication.java
@@ -0,0 +1,13 @@
+package com.security.jdbcsecurityoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcSecurityOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcSecurityOauthApplication.class, args);
+    }
+
+}
--- jdbc-security-oauth/src/main/java/com/security/jdbcsecurityoauth/config/AuthenticationConfiguration.java
+++ jdbc-security-oauth/src/main/java/com/security/jdbcsecurityoauth/config/AuthenticationConfiguration.java
@@ -1,28 +1,28 @@
-package com.uaa.securityoauth.config;
+package com.security.jdbcsecurityoauth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
 import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
 import org.springframework.security.oauth2.provider.code.InMemoryAuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
 import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import javax.sql.DataSource;
 import java.util.Collections;
 
 @Configuration
@@ -43,14 +43,19 @@ public class AuthenticationConfiguration extends AuthorizationServerConfigurerAd
 
     @Autowired
     private JwtAccessTokenConverter accessTokenConverter;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private DataSource dataSource;
     /**
-      * 授权码保存在内存中
+      * 授权码保存在数据库
       **/
     @Bean
-    public AuthorizationCodeServices authorizationCodeServices(){
-        return new InMemoryAuthorizationCodeServices();
+    public AuthorizationCodeServices authorizationCodeServices(DataSource dataSource){
+        return new JdbcAuthorizationCodeServices(dataSource);
     }
-
 
     @Bean
     public AuthorizationServerTokenServices tokenServices(){
@@ -90,20 +95,26 @@ public class AuthenticationConfiguration extends AuthorizationServerConfigurerAd
                 .allowFormAuthenticationForClients();//表单认证
     }
 
+    @Bean
+    public ClientDetailsService clientDetailsService(DataSource dataSource){
+        JdbcClientDetailsService clientDetailsService = new JdbcClientDetailsService(dataSource);
+        clientDetailsService.setPasswordEncoder(passwordEncoder);
+        return clientDetailsService;
+    }
     /**
       * 配置客户端信息
      * 如果是实际应用，这些数据应用来源于数据库之类的
       **/
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory()
-                .withClient("c1")
-                .secret("secret")
-                .resourceIds("order_resource")
-                .authorizedGrantTypes("password", "authorization_code", "refresh_token","client_credentials")
-                .scopes("all")
-                .autoApprove(false)// false 跳转到授权页面
-                .redirectUris("https://baidu.com/");
-
+        clients.withClientDetails(clientDetailsService);
+//        clients.inMemory()
+//                .withClient("c1")
+//                .secret("secret")
+//                .resourceIds("order_resource")
+//                .authorizedGrantTypes("password", "authorization_code", "refresh_token","client_credentials")
+//                .scopes("all")
+//                .autoApprove(false)// false 跳转到授权页面
+//                .redirectUris("https://baidu.com/");
     }
 }
