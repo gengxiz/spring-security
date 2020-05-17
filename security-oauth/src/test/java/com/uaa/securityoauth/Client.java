8ce5f8581ba8c18eae16e0ad8a03549cbcc71889
liyhu <2539517371@qq.com>
2020-05-17 20:11:50 +0800

    reset client

--- security-oauth/src/test/java/com/uaa/securityoauth/Client.java
+++ security-oauth/src/test/java/com/uaa/securityoauth/Client.java
@@ -5,5 +5,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class Client {
     public static void main(String[] args) {
         String encode = new BCryptPasswordEncoder().encode("123");
+        System.out.println(encode);
     }
 }
