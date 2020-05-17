02c6220edd667323e169bee462e16f36286fec84
liyhu <2539517371@qq.com>
2020-05-17 20:10:10 +0800

    update client

--- security-oauth/src/test/java/com/uaa/securityoauth/Client.java
+++ security-oauth/src/test/java/com/uaa/securityoauth/Client.java
@@ -5,6 +5,5 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class Client {
     public static void main(String[] args) {
         String encode = new BCryptPasswordEncoder().encode("123");
-        System.out.println(encode);
     }
 }
